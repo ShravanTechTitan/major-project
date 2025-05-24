da44cbca0729de6adb9a5d2808e3c5ba6d69eb54
ShravanTechTitan <shravankumar3849@gmail.com>
2025-05-25 01:19:33 +0530

    Update index.js

--- init/index.js
+++ init/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const initData = require("./data.js")
 const Listing = require("../models/listing.js");
 
-const mongo_url = 'mongodb://127.0.0.1:27017/wonderlust';
+const mongo_url = process.env.ATLASDB_URL
 
 main()
     .then((res)=>{
