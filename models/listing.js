8b5792bcfc31e5be8f25ba8852ff9ac9cd04960c
ShravanTechTitan <shravankumar3849@gmail.com>
2025-05-25 01:24:00 +0530

    Update listing.js

--- models/listing.js
+++ models/listing.js
@@ -34,11 +34,11 @@ const listingSchema = new Schema({
     type: {
       type: String, // Don't do `{ location: { type: String } }`
       enum: ['Point'], // 'location.type' must be 'Point'
-      required: true
+      // required: true
     },
     coordinates: {
       type: [Number],
-      required: true
+      // required: true
     }
   },
     reviews: [
